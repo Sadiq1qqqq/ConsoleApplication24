94ab15809780258ff551ef3968ec48f3097166f8
Sadiq Quliyev <sadiq000k@gmail.com>
2024-09-10 21:50:24 +0400

    Proje dosyası ekle.

---
+++ ConsoleApplication24/ConsoleApplication24.cpp
@@ -0,0 +1,163 @@
+﻿
+#include <iostream>
+using namespace std;
+//void array(int arr[], int len) {
+//	srand(time(NULL));
+//	int min = -30;
+//	int max = 50;
+//	for (int j = 0; j < len; j++)
+//	{
+//		int random = min + rand() % (max - min + 1);
+//		arr[j] = random;
+//	}
+
+//}
+//void array(float arr[], int len) {
+//	srand(time(NULL));
+//	int min = -3000;
+//	int max = 5000;
+//	for (int j = 0; j < len; j++)
+//	{
+//		float random = min + rand() % (max - min + 1);
+//		arr[j] = random / 100;
+//	}
+//
+//}
+//void createarray(double arr[], int len) {
+//	srand(time(NULL));
+//	int min = -30000000;
+//	int max = 50000000;
+//	for (int j = 0; j < len; j++)
+//	{
+//		double random = min + rand() % (max - min + 1);
+//		arr[j] = random / 100000;
+//	}
+//
+//}
+//void array(char arr[], int len) {
+//	srand(time(NULL));
+//	int min = 40;
+//	int max = 110;
+//	for (int j = 0; j < len; j++)
+//	{
+//		char random = min + rand() % (max - min + 1);
+//		arr[j] = random;
+//	}
+//
+//}
+
+
+
+//void array(int arr[], int len, int min, int max) {
+//	srand(time(NULL));
+//
+//	for (int j = 0; j < len; j++)
+//	{
+//		int random = min + rand() % (max - min + 1);
+//		arr[j] = random;
+//	}
+//}
+
+
+
+//void show(char arr[], int len) {
+//
+//	for (int j = 0; j < len; j++)
+//	{
+//		cout << arr[j] << " ";
+//	}
+//	cout << endl;
+//
+//}
+//void show(int arr[], int len) {
+//
+//	for (int j = 0;j < len;j++)
+//	{
+//		cout << arr[j] << " ";
+//	}
+//	cout << endl;
+//
+//}
+//void show(double arr[], int len) {
+//
+//	for (int j = 0; j < len; j++)
+//	{
+//		cout << arr[j] << " ";
+//	}
+//	cout << endl;
+//
+//}void showarray(float arr[], int len) {
+//
+//	for (int j= 0; j < len; j++)
+//	{
+//		cout << arr[j] << " ";
+//	}
+//	cout << endl;
+//
+//}
+
+
+//void sort(int arr[], int len, bool b) {
+//	int t;
+//	for (int i = 0; i < len - 1; i++)
+//	{
+//		for (int j = i + 1; j < len; j++)
+//		{
+//			if (b && arr[i] >= arr[j])
+//			{
+//				t = arr[i];
+//				arr[i] = arr[j];
+//				arr[j] = t;
+//			}
+//			if (!b && arr[i] <= arr[j])
+//			{
+//				t = arr[i];
+//				arr[i] = arr[j];
+//			
+//				arr[j] = t;
+//			
+//			}
+//		}
+//
+//
+//
+//
+//
+//	}
+//
+//
+//
+//
+//}
+
+//void ellement(int arr[], int len, int h) {
+//	for (int j = 0; j < len; j++)
+//	{
+//		if (arr[j] == h)
+//		{
+//			cout << "Elementin indeksi:" << j << endl;
+//		}
+//
+//	}
+//}
+//void findellement(float arr[], int len, float h) {
+//	for (int j = 0; j< len; j++)
+//	{
+//		if (arr[j] == h)
+//		{
+//			cout << "Elementin indeksi: " << j << endl;
+//		}
+//
+//	}
+//}
+
+//
+//void ellement(double arr[], int len, double h) {
+//	for (int j = 0; j < len; j++)
+//	{
+//		if (arr[j] == h)
+//		{
+//			cout << "Elementin indeksi: " << j << endl;
+//		}
+//
+//	}
